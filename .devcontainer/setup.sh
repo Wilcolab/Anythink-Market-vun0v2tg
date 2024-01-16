85268b3c2750b672bb1346303bca321ff7027583
Dar Malovani <dar.malovani@wilcohq.com>
2024-01-16 16:28:48 +0200

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -13,7 +13,7 @@ echo "export CODESPACE_WDS_SOCKET_PORT=443" >> ~/.bashrc
 
 
 # Change backend port visibility to public
-echo "(&>/dev/null .devcontainer/open_port.sh >> /tmp/ports.log &)" >> ~/.bashrc
+.devcontainer/open_port.sh >> /tmp/ports.log
 
 
 # Export welcome prompt in bash:
