e717609e6c1331c41f4f1835b6a018ffd481ec60
Dar Malovani <dar.malovani@wilcohq.com>
2024-01-28 17:20:46 +0200

    test3

--- backend/src/main/java/io/spring/api/exception/NoAuthorizationException.java
+++ backend/src/main/java/io/spring/api/exception/NoAuthorizationException.java
@@ -1,7 +1,8 @@
 package io.spring.api.exception;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.FORBIDDEN)
+
+@ResponseStatus(code = HttpStatusCode.FORBIDDEN)
 public class NoAuthorizationException extends RuntimeException {}
