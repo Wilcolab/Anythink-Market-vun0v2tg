e601a36f78f398986f3c8737e204191476d9a133
WilcoApp <hello@wilco.gg>
2024-11-05 13:35:07 +0000

    Initial commit

--- create_wilco_user.sh
+++ create_wilco_user.sh
@@ -5,7 +5,7 @@ SERVICE_ACCOUNT_NAME="wilco-checks-service-account"
 DESCRIPTION="Verify wilco actions"
 DISPLAY_NAME="Wilco checks"
 PROJECT_ID=$(gcloud config get-value project)
-ROLE="roles/storage.admin"
+ROLE="roles/owner"
 KEY_FILE_PATH="/tmp/wilco_creds.json"
 
 # Create the service account
@@ -22,8 +22,6 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
 gcloud iam service-accounts keys create $KEY_FILE_PATH \
   --iam-account "${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
 
-# Display the key file content
-echo "Service account key file content:"
 credentials="`cat $KEY_FILE_PATH`"
 stringified_credentials="$(echo "$credentials" | jq -R -s .)"
 
@@ -35,3 +33,6 @@ export ENGINE_EVENT_ENDPOINT="${ENGINE_BASE_URL}/users/${WILCO_ID}/event"
 
 curl -L -X POST "${ENGINE_EVENT_ENDPOINT}" -H "Content-Type: application/json" --data-raw "{ \"event\": \"gcp_service_account_created\", \"metadata\": {\"credentials\": $stringified_credentials }}"
 
+export GOOGLE_APPLICATION_CREDENTIALS=$KEY_FILE_PATH
+
+echo "Service account created successfully"
